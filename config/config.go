59dd21f6b6319d41e4a1e30f987356d7c6553a2e
Greg Dean <majorthorn@gmail.com>
2019-03-19 22:48:50 -0500

    Removed old println from config.go

--- config/config.go
+++ config/config.go
@@ -29,12 +29,13 @@ func ReadConfig() error {
 		fmt.Println(err.Error())
 		return err
 	}
-	//fmt.Println(string(file))
+	
 	err = json.Unmarshal(file, &config)
 	if err != nil {
 		fmt.Println(err.Error())
 		return err
 	}
+	
 	Token = config.Token
 	BotPrefix = config.BotPrefix
 	return nil
